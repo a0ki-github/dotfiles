7882480be8b14818e69501770c9892d11dd812bd
a0ki-github <aoki.hotaka@gmail.com>
2024-01-03 21:49:08 +0900

    add: 補完系プラグインの導入

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,30 +3,43 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require("packer").startup(function(use)
-  -- Packer can manage itself
-  use "wbthomason/packer.nvim"
+return require("packer").startup(
+  function(use)
+    -- Packer can manage itself
+    use "wbthomason/packer.nvim"
 
-  -- コメントアウトのショートカット
-  use "tomtom/tcomment_vim"
+    -- コメントアウトのショートカット
+    use "tomtom/tcomment_vim"
 
-  -- 括弧やブロックなどのペア間のジャンプなど
-  use {"andymass/vim-matchup", event = "VimEnter"}
+    -- 括弧やブロックなどのペア間のジャンプなど
+    use {"andymass/vim-matchup", event = "VimEnter"}
 
-  -- コミット履歴などの確認
-  use { "rhysd/git-messenger.vim", opt = true, cmd = { "GitMessenger" } }
-  
-  -- ファイルあいまい検索
-  use { "ibhagwan/fzf-lua",
-    -- optional for icon support
-    requires = { "nvim-tree/nvim-web-devicons" },
-  }
+    -- コミット履歴などの確認
+    use { "rhysd/git-messenger.vim", opt = true, cmd = { "GitMessenger" } }
 
-  -- lsp
-  use {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
-  }
-  require("lspconfig")
-end)
+    -- ファイルあいまい検索
+    use { "ibhagwan/fzf-lua",
+      -- optional for icon support
+      requires = { "nvim-tree/nvim-web-devicons" },
+    }
+
+    -- TODO 全角スペースの可視化
+
+    -- lsp
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+
+    require("lspconfig")
+
+    use "hrsh7th/nvim-cmp" --補完エンジン本体
+    use "hrsh7th/cmp-nvim-lsp" --LSPを補完ソースに
+    use "hrsh7th/cmp-buffer"  --bufferを補完ソースに
+    use "hrsh7th/cmp-path"  --pathを補完ソースに
+    use "hrsh7th/vim-vsnip" --スニペットエンジン
+    use "hrsh7th/cmp-vsnip" --スニペットを補完ソースに
+
+    require("cmp")
+  end)
