672a07a386b9206b84560d4b35fd10b4c208e3cc
a0ki-github <aoki.hotaka@gmail.com>
2024-03-06 09:25:55 +0900

    add: MarkdownPreview

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -34,6 +34,14 @@ return require("packer").startup(
         end,
     }
 
+    -- マークダウンプレビュー
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
+
+    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
     -- lsp
     use {
         "williamboman/mason.nvim",
