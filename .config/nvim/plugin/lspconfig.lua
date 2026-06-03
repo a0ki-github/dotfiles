2916afd417376952eef461b713e2c3de398ddc35
a0ki-github <aoki.hotaka@gmail.com>
2026-06-03 19:12:15 +0900

    add: Masonインストールをコード管理

--- .config/nvim/plugin/lspconfig.lua
+++ .config/nvim/plugin/lspconfig.lua
@@ -1,4 +1,40 @@
+local mason_packages = vim.fn.stdpath('data') .. '/mason/packages'
+local vue_language_server = mason_packages .. '/vue-language-server/node_modules/@vue/language-server'
+
 require("mason").setup()
+
+vim.diagnostic.config({
+  virtual_text = true,
+})
+
+-- Vue (Volar) は hybrid mode 仕様: vue_ls に tsdk を渡し、TS は ts_ls + @vue/typescript-plugin が担当する
+vim.lsp.config('vue_ls', {
+  init_options = {
+    typescript = {
+      tsdk = mason_packages .. '/vue-language-server/node_modules/typescript/lib',
+    },
+  },
+})
+
+vim.lsp.config('ts_ls', {
+  init_options = {
+    plugins = {
+      {
+        name = '@vue/typescript-plugin',
+        location = vue_language_server .. '/node_modules/@vue/typescript-plugin',
+        languages = { 'vue' },
+      },
+    },
+  },
+  filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue' },
+})
+
 require("mason-lspconfig").setup({
+  ensure_installed = {
+    "basedpyright",
+    "vue_ls",
+    "ts_ls",
+    "intelephense",
+  },
   automatic_enable = true,
 })
